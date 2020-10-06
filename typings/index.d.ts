1a3608a411c9f5927fa2b3e45dca93366bfbdf0e
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2020-10-07 09:47:39 +1100

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,17 @@
 export interface message {
-  send(message: Message | string);
-  hover(message: message | string, hover: message | string);
-  sendDialogue(messages: (message | string)[]);
+  send(message: Message | string): void;
+  hover(message: message | string, hover: message | string): void;
+  sendDialogue(messages: (Message | string)[]): void;
 }
 
 export class Message {
   message: string;
   constructor(message: string);
+}
+
+export function on(event: EventType, callback: Function): void;
+
+export enum EventType {
+  JOIN,
+  LEAVE,
 }
