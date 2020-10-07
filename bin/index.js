d9652de206dbbc38cb84c2e3e46876bfe5015151
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-07 19:36:39 +1300

    a

--- bin/index.js
+++ bin/index.js
@@ -9,17 +9,35 @@ const args = process.argv.slice(2);
 const blocks = require(path.join(process.cwd(), args[0])).blocks;
 
 let code = [
-  { block: "event", action: "Join" },
-  { block: "player_action", action: "EnableBlocks" },
+  {
+    block: "event",
+    action: "Join",
+    args: [],
+  },
+  {
+    block: "player_action",
+    action: "SendMessage",
+    args: [{ id: "txt", name: "Hello, World" }],
+    target: "Default",
+  },
 ];
 
-gzip(
-  `{"blocks":[${code
-    .map(
-      (m) =>
-        `{"id":"block","block":"${m.block}","args":{"items":[]},"action":"${m.action}"}`
-    )
-    .join(",")}]}`
-).then((compressed) =>
+let uncompressed = `{"blocks":[`;
+
+code.forEach((m) => {
+  var i = 0;
+
+  uncompressed += `{"id":"block","block":"${
+    m.block
+  }","args":{"items":[${m.args.map(
+    (n) => `{"id":"${n.id}","data":{"name":"${n.name}"}},"slot":${i++}}`
+  )}]},"action":"${m.action}"${m.target ? `,"target":"${m.target}"` : ""}},`;
+});
+
+uncompressed = uncompressed.substring(0, uncompressed.length - 1);
+
+uncompressed += `]}`;
+
+gzip(uncompressed).then((compressed) =>
   console.log(new Buffer.from(compressed).toString("base64"))
 );
