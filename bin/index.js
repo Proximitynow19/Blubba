a884823e1d196f9f3fbd5f044e65f935160a7723
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2020-10-07 14:22:29 +1100

    i want thy die

--- bin/index.js
+++ bin/index.js
@@ -6,11 +6,9 @@ const { gzip } = require("node-gzip");
 
 const args = process.argv.slice(2);
 
-const file = fs.readFileSync(path.join(process.cwd(), args[0]));
+const response = require(path.join(process.cwd(), args[0]));
 
 let output = { blocks: [] };
-
-console.log(file.toString());
 
 gzip("output").then((compressed) =>
   console.log(new Buffer.from(compressed).toString("base64"))
