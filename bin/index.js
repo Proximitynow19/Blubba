5f22dc73a34485a971a570ffbe07552c609485ea
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-07 16:37:20 +1300

    a

--- bin/index.js
+++ bin/index.js
@@ -8,10 +8,17 @@ const args = process.argv.slice(2);
 
 const file = fs.readFileSync(path.join(process.cwd(), args[0]));
 
-let output = { blocks: [] };
+let events = [];
 
 console.log(file.toString());
 
-gzip("output").then((compressed) =>
+gzip(
+  `{"blocks":[${events
+    .map(
+      (m) =>
+        `{"id":"block","block":"func","args":{"items":[{"item":{"id":"item","data":{"item":"{DF_NBT:2580,id:\"minecraft:ender_chest\",tag:{PublicBukkitValues:{\"hypercube:codetemplatedata\":'{\"author\":\"Proximitynow\",\"name\":\"§bCode Template §3» §bEvent\",\"version\":1,\"code\":\"${m}\"}'},display:{Name:'{\"extra\":[{\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"color\":\"aqua\",\"text\":\"Code Template \"},{\"italic\":false,\"color\":\"dark_aqua\",\"text\":\"» \"},{\"italic\":false,\"color\":\"aqua\",\"text\":\"Event\"}],\"text\":\"\"}'}},Count:1b}"}},"slot":0}`
+    )
+    .join(",")},"data":""}]}`
+).then((compressed) =>
   console.log(new Buffer.from(compressed).toString("base64"))
 );
