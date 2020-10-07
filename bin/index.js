e84511e9af416ba24a7ffb2dd3058998f1c0c609
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-07 13:08:13 +1300

    a

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
 
+const path = require("path");
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+const file = fs.readFileSync(path.join(process.cwd(), args[0]));
+
+console.log(file.toString());
