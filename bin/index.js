a44aa5fbc2cb9bcd9295f81c01da8e5546b02920
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-07 14:04:03 +1300

    Compression

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,12 @@
 
 const path = require("path");
 const fs = require("fs");
+const { gzip } = require("node-gzip");
 
 const args = process.argv.slice(2);
 
 const file = fs.readFileSync(path.join(process.cwd(), args[0]));
 
 console.log(file.toString());
+
+return gzip(output).then((compressed) => new Buffer(compressed).toString("base64"));
