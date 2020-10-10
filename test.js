fc8ee6ed0aed527c7c4f9701dc35401bdc3fb350
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-10 19:28:12 +1300

    a

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const blubba = require("./lib/index");
 
 blubba.on("JOIN", () => {
   blubba.player_action.SENDMESSAGE("Hello World!");
+  blubba.set_var.set("v_test", "123321");
+  blubba.player_action.SENDMESSAGE("v_test");
 });
 
 module.exports = blubba.blocks;
