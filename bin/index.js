f7016679c1f4a3bc88bb0d13a3dd3dc5822c9443
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-10-10 15:54:30 +1300

    a

--- bin/index.js
+++ bin/index.js
@@ -1,55 +1,12 @@
 #!/usr/bin/env node
 
 const path = require("path");
-const fs = require("fs");
 const { gzip } = require("node-gzip");
+
 const args = process.argv.slice(2);
-const blocks = require(path.join(process.cwd(), args[0])).blocks;
 
-let code = [
-  { block: "event", action: "Quit", args: [] },
-  {
-    block: "player_action",
-    action: "SendMessage",
-    args: [
-      {
-        id: "txt",
-        name: "Hello World!",
-      },
-    ],
-  },
-];
+const blocks = require(path.join(process.cwd(), args[0]));
 
-let uncompressed = { blocks: [] };
-
-var block = 0;
-
-code.forEach((m) => {
-  var i = 0;
-
-  uncompressed.blocks.push({
-    id: "block",
-    block: m.block,
-    args: {
-      items: m.args.map((n) => ({
-        item: {
-          id: n.id,
-          data: { name: n.name },
-        },
-        slot: i++,
-      })),
-    },
-    action: m.action,
-  });
-
-  m.args.forEach((element, index) => {
-    if (element.target)
-      uncompressed.blocks[block].args[index][target] = element.target;
-  });
-
-  block++;
-});
-
-gzip(JSON.stringify(uncompressed)).then((compressed) =>
+gzip(JSON.stringify({ blocks })).then((compressed) =>
   console.log(new Buffer.from(compressed).toString("base64"))
 );
